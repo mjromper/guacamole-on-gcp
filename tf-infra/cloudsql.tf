d543ab67ddcc5386488d1f951bb18ee4369a4958
Manuel Romero <mjromper@gmail.com>
2024-02-15 12:21:43 +0100

    keystore

--- tf-infra/cloudsql.tf
+++ tf-infra/cloudsql.tf
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+/*
 resource "google_service_networking_connection" "private_vpc_connection" {
   provider                = google
   network                 = google_compute_network.vpc.id
@@ -21,11 +22,6 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 }
 
 resource "google_sql_database_instance" "guacamole-mysql" {
-  /*
-    Random instance name needed because:
-    "You cannot reuse an instance name for up to a week after you have deleted an instance."
-    See https://cloud.google.com/sql/docs/mysql/delete-instance for details.
-    */
   name             = "${var.db_name}-${random_id.suffix.hex}"
   database_version = "MYSQL_5_7"
   region           = var.region
@@ -59,6 +55,7 @@ resource "google_sql_ssl_cert" "db-client-cert" {
   common_name = "guac-db-client-cert-${random_id.suffix.hex}"
   instance    = google_sql_database_instance.guacamole-mysql.name
 }
+*/
 
 data "external" "db-client-cert-keystore" {
   program     = ["bash", "${path.module}/tf-infra/bin/generate-keystore.sh"]
@@ -66,9 +63,9 @@ data "external" "db-client-cert-keystore" {
 
   query = {
     keystore_password = "${random_password.keystore_password.result}"
-    common_name       = "${google_sql_ssl_cert.db-client-cert.common_name}"
-    server_ca_cert    = "${google_sql_ssl_cert.db-client-cert.server_ca_cert}"
-    cert              = "${google_sql_ssl_cert.db-client-cert.cert}"
-    private_key       = "${google_sql_ssl_cert.db-client-cert.private_key}"
+    #common_name       = "${google_sql_ssl_cert.db-client-cert.common_name}"
+    #server_ca_cert    = "${google_sql_ssl_cert.db-client-cert.server_ca_cert}"
+    #cert              = "${google_sql_ssl_cert.db-client-cert.cert}"
+    #private_key       = "${google_sql_ssl_cert.db-client-cert.private_key}"
   }
 }
