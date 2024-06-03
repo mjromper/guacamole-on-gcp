7cb7d5654afed42686efaec659534885368c4606
Manuel Romero <mjromper@gmail.com>
2024-06-03 11:14:51 +0200

    prod

--- tf-infra/compute.tf
+++ tf-infra/compute.tf
@@ -48,7 +48,7 @@ resource "google_compute_router" "router" {
 }
 
 resource "google_compute_router_nat" "nat" {
-  name                               = "guacamole-router-nat"
+  name                               = "guacamole-router-nat-prod"
   router                             = google_compute_router.router.name
   region                             = google_compute_router.router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
